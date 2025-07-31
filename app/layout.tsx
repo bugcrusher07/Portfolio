be6d9ac6e020269b6341229d256cc0540a2b4a4c
bugcruher07 <tarun.develops@gmail.com>
2025-07-31 20:24:00 +0530

    changing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Portfolio-Tarun',
+  description: 'This fullstack project/portfolio displays all my skills through live and deployed projects',
 }
 
 export default function RootLayout({
